c2668b085ba6240644761d3e03a58f73f2795840
Vinicius Costa de Abreu <vinicius.vc67@gmail.com>
2025-10-27 16:41:06 +0000

    Utilizando condições em javascript

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60) {
+    console.log('VOce ultrapassou a velocidade permitida. Multado!!!')
+}
+console.log('Dirija sempre com cinto de segurança')
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vinvendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Voce é brasileiro!')
+}else{
+    console.log('Voce é estrangeiro!')
+}
