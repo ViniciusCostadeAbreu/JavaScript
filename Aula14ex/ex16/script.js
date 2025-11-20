2194fe4fbfb8f0025303d8ff532d42e19454d9ef
Vinicius Costa de Abreu <vinicius.vc67@gmail.com>
2025-11-20 16:38:43 +0000

    Exercicio 16 concluido

--- Aula14ex/ex16/script.js
+++ Aula14ex/ex16/script.js
@@ -4,14 +4,28 @@ function contar(){
     var pas = document.getElementById('txtpas')
     var res = document.getElementById('res')
     res.innerHTML = ''
-    if (ini.value == '' || fim.value == '' || pas.value == ''){
+    if ((ini.value == '' || fim.value == '' || pas.value == '')){
         window.alert('Por Favor informe os valores')
-    }else{
+    } else if (ini.value < 0 || fim.value < 0 || pas.value < 0){
+        window.alert('Não São permitidos Numeros Negativos')
+    }else if (ini.value > fim.value){
+        window.alert('O valor inical não pode ser maior que o valor final')
+    }
+    else{
         if(Number(pas.value) == 0){
-            var c = Number(ini.value) 
-            while(c < (Number(fim.value) +1)){
+            //var c = Number(ini.value) 
+            for(var c = Number(ini.value); c < (Number(fim.value) +1); c++){
+                res.innerHTML = res.innerHTML + '👉' + c 
+            }
+
+            //ASSIM TAMBEM FUNCIONA
+            /*while(c < (Number(fim.value) +1)){
                 res.innerHTML = res.innerHTML + '👉' + c 
                 c++
+            }*/
+        }else {
+            for(var c = Number(ini.value); c < (Number(fim.value) +1); c+=Number(pas.value)){
+                res.innerHTML = res.innerHTML + '👉' + c 
             }
         }
         
