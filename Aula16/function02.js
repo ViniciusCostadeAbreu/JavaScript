9a7d2b757ade88e6d7c88ddf4fa39ba04e38c635
Vinicius Costa de Abreu <vinicius.vc67@gmail.com>
2025-12-18 18:06:54 +0000

    aula 16 finalizada

---
+++ Aula16/function02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0){ //O 0 na frente faz com que o parametro tenha um vaor pre definido caso não seja mandado na chamada da função
+    return n1 + n2
+}
+
+console.log(soma(5, 8))
