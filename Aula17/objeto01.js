ebf2f69756912df88d22e407ccb20d9dc0c9cd7e
Vinicius Costa de Abreu <vinicius.vc67@gmail.com>
2026-01-07 17:41:53 +0000

    estudo de objetos

---
+++ Aula17/objeto01.js
@@ -0,0 +1,11 @@
+let amigo = {
+    nome: 'José',
+    sexo: 'M',
+    peso: 85.4,
+    engordar(p=0){
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+
+console.log(`O ${amigo.nome}, pesa ${engordar(2)}`)
